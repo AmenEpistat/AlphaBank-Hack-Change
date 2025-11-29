5d833647cb2eb59d1b9c4c79e0679b8159acfba0
FeudeyTF <yanmordanenko089@gmail.com>
2025-11-29 18:57:20 +0300

    Вместо заглушки теперь полноценная модель от Марка

--- backend/AlphaStructure/Clients/IClient.cs
+++ backend/AlphaStructure/Clients/IClient.cs
@@ -2,6 +2,26 @@
 {
     public interface IClient
     {
-        public string Id { get; set; }
+        public string Id { get; }
+
+        public float Age { get; }
+
+        public Gender Gender { get; }
+
+        public float LogSalary6to12 { get; }
+
+        public bool IsSalaryKnow { get; }
+
+        public float PerCapitaIncomeInRegion { get; }
+
+        public float LogTurnOtherCrAvgAct { get; }
+
+        public bool IsNaTurnOtherCrAvgAct { get; }
+
+        public float TransactionCategorySupermarketPercentCnt { get; }
+
+        public float LogTransactionCategoryRestaurantsSumAmt { get; }
+
+        public float LogAvgByCategoryAmountSumCashflowcategoryName { get; }
     }
 }
--- backend/AlphaStructure/Entities/AlphaBankClient.cs
+++ backend/AlphaStructure/Entities/AlphaBankClient.cs
@@ -4,11 +4,52 @@ namespace AlphaOfferService.AlphaStructure.Entities
 {
     public class AlphaBankClient : IClient
     {
-        public string Id { get; set; }
+        public string Id { get; }
 
-        public AlphaBankClient(string id)
+        public float Age { get; }
+
+        public Gender Gender { get; }
+
+        public float LogSalary6to12 { get; }
+
+        public bool IsSalaryKnow { get; }
+
+        public float PerCapitaIncomeInRegion { get; }
+
+        public float LogTurnOtherCrAvgAct { get; }
+
+        public bool IsNaTurnOtherCrAvgAct { get; }
+
+        public float TransactionCategorySupermarketPercentCnt { get; }
+
+        public float LogTransactionCategoryRestaurantsSumAmt { get; }
+
+        public float LogAvgByCategoryAmountSumCashflowcategoryName { get; }
+
+        public AlphaBankClient(
+            string id,
+            float age,
+            Gender gender,
+            float logSalary6to12,
+            bool isSalaryKnow,
+            float perCapitaIncomeInRegion,
+            float logTurnOtherCrAvgAct,
+            bool isNaTurnOtherCrAvgAct,
+            float transactionCategorySupermarketPercentCnt,
+            float logTransactionCategoryRestaurantsSumAmt,
+            float logAvgByCategoryAmountSumCashflowcategoryName)
         {
             Id = id;
+            Age = age;
+            Gender = gender;
+            LogSalary6to12 = logSalary6to12;
+            IsSalaryKnow = isSalaryKnow;
+            PerCapitaIncomeInRegion = perCapitaIncomeInRegion;
+            LogTurnOtherCrAvgAct = logTurnOtherCrAvgAct;
+            IsNaTurnOtherCrAvgAct = isNaTurnOtherCrAvgAct;
+            TransactionCategorySupermarketPercentCnt = transactionCategorySupermarketPercentCnt;
+            LogTransactionCategoryRestaurantsSumAmt = logTransactionCategoryRestaurantsSumAmt;
+            LogAvgByCategoryAmountSumCashflowcategoryName = logAvgByCategoryAmountSumCashflowcategoryName;
         }
     }
 }
